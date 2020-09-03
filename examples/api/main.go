824ff7dbb0835de86e181bc4385c5b53facf9987
asakiz <asakizin@gmail.com>
2020-09-03 16:05:16 -0300

    Update API to agent v2
    Signed-off-by: asakiz <asakizin@gmail.com>

---
+++ examples/api/main.go
@@ -0,0 +1,97 @@
+/*
+Copyright (C) 2019
+O.S. Systems Sofware LTDA: contato@ossystems.com.br
+
+SPDX-License-Identifier: Apache-2.0
+*/
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/UpdateHub/agent-sdk-go"
+)
+
+func main() {
+	client := updatehub.NewClient()
+
+	info, err := client.GetInfo()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	infoResponse, err := json.Marshal(info)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(infoResponse))
+
+	probe, err := client.Probe("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	probeResponse, err := json.Marshal(probe)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(probeResponse))
+
+	probeCustom, err := client.Probe("http://www.example.com:8080")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	probeCustomResponse, err := json.Marshal(probeCustom)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(probeCustomResponse))
+
+	logs, err := client.GetLogs()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logResponse, err := json.Marshal(logs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(logResponse))
+
+	remoteInstall, err := client.RemoteInstall("https://foo.bar/update.uhu")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	remoteInstallResponse, err := json.Marshal(remoteInstall)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(remoteInstallResponse))
+
+	localInstall, err := client.LocalInstall("/tmp/update.uhu")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	localInstallResponse, err := json.Marshal(localInstall)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(localInstallResponse))
+}
---
+++ examples/listener/main.go
@@ -0,0 +1,45 @@
+/*
+Copyright (C) 2019
+O.S. Systems Sofware LTDA: contato@ossystems.com.br
+
+SPDX-License-Identifier: Apache-2.0
+*/
+package main
+
+import (
+	"fmt"
+	"log"
+
+	updatehub "github.com/UpdateHub/agent-sdk-go"
+)
+
+func main() {
+	l := updatehub.NewStateChangeListener()
+	l.On(updatehub.StateDownload, func(state *updatehub.State) {
+		if state.ID == updatehub.StateDownload {
+			fmt.Println("downloading state")
+		}
+		fmt.Println("Canceling the command...")
+		state.Cancel()
+		fmt.Println("Done")
+	})
+
+	l.On(updatehub.StateError, func(state *updatehub.State) {
+		if state.ID == updatehub.StateError {
+			fmt.Println("Error")
+		}
+		state.Proceed()
+		fmt.Println("Done")
+	})
+
+	l.On(updatehub.StateReboot, func(state *updatehub.State) {
+		if state.ID == updatehub.StateReboot {
+			fmt.Println("rebooting...")
+		}
+	})
+
+	err := l.Listen()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
